7d3fe86b1e634c2bdd5da7bdf089cc1c08a8ef53
MEGA <MEGA@gmail.com>
2025-03-14 18:18:32 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> numbers = new LinkedList<int>();
+            numbers.AddLast(7);
+            numbers.AddLast(42);
+            numbers.AddLast(19);
+            numbers.AddLast(56);
+            numbers.AddLast(88);
+            numbers.AddLast(33);
+            List<int> sortednumbers = new List<int>(numbers);
+            sortednumbers.Sort();
+            LinkedList<int> sortedList = new LinkedList<int>(sortednumbers);
+            Console.WriteLine("Podreden spisyk");
+            foreach (int num in sortedList)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
